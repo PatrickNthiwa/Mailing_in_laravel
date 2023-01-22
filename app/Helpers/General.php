af62d2ff2a8f62ba42f6173a4e45d70a3442510e
PatrickNthiwa <pwnthiwa@gmail.com>
2023-01-22 17:20:47 +0300

    edited generals helper class

--- app/Helpers/General.php
+++ app/Helpers/General.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Setting;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * upload file
@@ -90,5 +91,23 @@ function checkDirectory($directory)
  */
 function user_can($permission)
 {
-    return \Auth::user()->is_admin == 1 || \Auth::user()->can($permission);
+    return Auth::user()->is_admin == 1 || Auth::user()->can($permission);
+    function getUnreadMessages()
+    {
+        $folder = \App\Models\MailboxFolder::where('title', "Inbox")->first();
+        $messages = \App\Models\Mailbox::join('mailbox_receiver', 'mailbox_receiver.mailbox_id', '=', 'mailbox.id')
+            ->join('mailbox_user_folder', 'mailbox_user_folder.user_id', '=', 'mailbox_receiver.receiver_id')
+            ->join('mailbox_flags', 'mailbox_flags.user_id', '=', 'mailbox_user_folder.user_id')
+            ->where('mailbox_receiver.receiver_id', Auth::user()->id)
+//                          ->where('parent_id', 0)
+            ->where('mailbox_flags.is_unread', 1)
+            ->where('mailbox_user_folder.folder_id', $folder->id)
+            ->where('sender_id', '!=',Auth::user()->id)
+            ->whereRaw('mailbox.id=mailbox_receiver.mailbox_id')
+            ->whereRaw('mailbox.id=mailbox_flags.mailbox_id')
+            ->whereRaw('mailbox.id=mailbox_user_folder.mailbox_id')
+            ->select(["*", "mailbox.id as id"])
+            ->get();
+        return $messages;
+    }
 }
