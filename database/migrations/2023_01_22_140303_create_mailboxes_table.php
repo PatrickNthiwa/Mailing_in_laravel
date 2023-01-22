f8d01543c1bfdb8c71acbb26431b53f0cee919ca
PatrickNthiwa <pwnthiwa@gmail.com>
2023-01-22 17:07:27 +0300

    edited the migrations for mailbox

--- database/migrations/2023_01_22_140303_create_mailboxes_table.php
+++ database/migrations/2023_01_22_140303_create_mailboxes_table.php
@@ -13,9 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('mailboxes', function (Blueprint $table) {
-            $table->id();
+        Schema::create('mailbox', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("subject");
+            $table->longText("body")->nullable();
+            $table->integer("sender_id")->unsigned();
+            $table->string("time_sent");
+            $table->integer("parent_id")->default(0);
             $table->timestamps();
+            $table->foreign('sender_id')->references('id')->on('users');
         });
     }
 
