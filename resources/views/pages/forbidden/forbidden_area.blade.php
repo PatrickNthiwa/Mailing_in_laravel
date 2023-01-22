28902734c00b30caa05d0087364af400c54f08db
PatrickNthiwa <pwnthiwa@gmail.com>
2023-01-22 16:28:27 +0300

    added the forbiden area and added the adminmiddleware to the kernel

---
+++ resources/views/pages/forbidden/forbidden_area.blade.php
@@ -0,0 +1,24 @@
+@extends('layout.app')
+
+@section('title', ' | Forbidden')
+
+@section('content')
+
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Forbidden area
+        </h1>
+    </section>
+
+    <section class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <p>Trying to access forbidden area</p>
+            </div>
+        </div>
+    </section>
+
+
+
+@stop
